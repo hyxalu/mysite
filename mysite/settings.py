ad50f48f8197c60fd33923ffa1a32eb073b59201
david <schaillie@gmail.com>
2018-06-03 21:41:41 +0200

    Travail en cours

--- mysite/settings.py
+++ mysite/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'umm',
+    'umm.apps.UmmConfig',
 ]
 
 MIDDLEWARE = [
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Brussels'
 
 USE_I18N = True
 
--- umm/admin.py
+++ umm/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Evenement, Song
+
+
+admin.site.register(Evenement)
+admin.site.register(Song)
