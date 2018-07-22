837f9547b52e2de417eccd64586e46a551c89f2a
david <schaillie@gmail.com>
2018-07-22 08:19:07 +0200

    Customize navbar

--- umm/views.py
+++ umm/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+
+from .models import Evenement
 
 # Create your views here.
 def index(request):
-    return render(request, 'umm/index.html')
+    future_events_list = Evenement.objects.filter(date__gte=timezone.now()).order_by('date')
+    context = {'future_events': future_events_list}
+    return render(request, 'umm/index.html', context)
