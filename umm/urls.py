45f62628771da7383e3ce6ce6caf1cfcbf4312af
david <schaillie@gmail.com>
2018-08-03 13:05:36 +0200

    Navigate with menu

--- umm/urls.py
+++ umm/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = 'umm'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('email/', views.emailView, name='email'),
